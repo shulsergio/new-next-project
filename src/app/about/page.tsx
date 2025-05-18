7ab5bf39063305ff0d9c79c51cdabe184da3235c
Sergey Shulga <shul.sergio@gmail.com>
2025-05-18 13:40:29 +0300

    start next app

---
+++ src/app/about/page.tsx
@@ -0,0 +1,28 @@
+import css from "./about.module.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About My App",
+  description: "This is the about app page.",
+  keywords: ["about", "app", "information"],
+  authors: [{ name: "Shulcman" }],
+  robots: {
+    index: true,
+    follow: true,
+  },
+};
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+};
+
+export default function About() {
+  return (
+    <>
+      <div className={css.textdata}>
+        <p className={css.text}>ABOUT</p>
+      </div>
+    </>
+  );
+}
