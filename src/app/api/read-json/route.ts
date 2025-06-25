be598631e626e8c726c2e1a105150924b10f1008
Sergey Shulga <shul.sergio@gmail.com>
2025-06-26 02:07:10 +0300

    add TopBonusList + draw top bonuses

--- src/app/api/read-json/route.ts
+++ src/app/api/read-json/route.ts
@@ -2,13 +2,37 @@ import { NextResponse } from 'next/server';
 import path from 'path';
 import fs from 'fs/promises';
 
-export async function GET() {
+export async function GET(request: Request) {
  try {
+const { searchParams } = new URL(request.url);
+console.log('///// searchParams:', searchParams);
+const shopId = searchParams.get('shopId');
+const userType = searchParams.get('userType');
+
+console.log('///// shopId:', shopId);
+console.log('///// userType:', userType);
+
+
+ if (!shopId) {
+      return NextResponse.json(
+        { message: 'Shop ID parameter is missing' },
+        { status: 400 }
+      );
+    }
+    if (!userType) { 
+      return NextResponse.json(
+        { message: 'User Type parameter is missing' },
+        { status: 400 }
+      );
+    }
 
      const jsonFilePath = path.join(process.cwd(), 'public', 'bonus.json');
      const fileContent = await fs.readFile(jsonFilePath, 'utf-8');
-     const data = JSON.parse(fileContent);
-    return NextResponse.json(data);
+
+     const data: []= JSON.parse(fileContent);
+     const filteredData = data.filter(item =>item['Site Id']=== shopId && item['Type']=== userType);
+
+    return NextResponse.json(filteredData);
 
  } catch (error) {
     return NextResponse.json(
--- src/app/user/plans/top-bonus/page.tsx
+++ src/app/user/plans/top-bonus/page.tsx
@@ -1,9 +1,10 @@
 import ComponentWrapper from "@/components/ComponentWrapper/ComponentWrapper";
+import TopBonusList from "@/components/TopBonusList/TopBonusList";
 
 export default function UserTopBonusPage(){
     return(
         <ComponentWrapper title="Top Bonus list">
-list
+<TopBonusList/>
 
         </ComponentWrapper>
 
