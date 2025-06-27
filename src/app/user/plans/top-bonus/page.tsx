9c1efce5abc48bfd3f8d3f6bfd1833276fed387a
Sergey Shulga <shul.sergio@gmail.com>
2025-06-28 02:48:49 +0300

    add button @back@ in TopBonus

--- src/app/user/plans/top-bonus/page.tsx
+++ src/app/user/plans/top-bonus/page.tsx
@@ -1,12 +1,13 @@
+import ButtonBox from "@/components/ButtonBox/ButtonBox";
 import ComponentWrapper from "@/components/ComponentWrapper/ComponentWrapper";
 import TopBonusList from "@/components/TopBonusList/TopBonusList";
 
 export default function UserTopBonusPage(){
-    return(
+    return(<>
         <ComponentWrapper title="Top Bonus list">
 <TopBonusList/>
 
         </ComponentWrapper>
-
-    )
+<ButtonBox option="link" href="/user/plans/" >Back</ButtonBox>
+</>    )
 }
