a422350ff0c342f8e4c466357818abccbb3328b2
Sergey Shulga <shul.sergio@gmail.com>
2025-08-03 23:58:35 +0300

    add fetchShopsById for shop name

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -186,3 +186,30 @@ export async function fetchShopMatixData(storeId: string, accessToken: string) {
     console.log('%%%% FILE fetchShopMatixData: ', data)
     return data;
   }
+
+
+  export async function fetchShopsById(storeId: string, accessToken: string){
+
+    const BackApi = `${process.env.NEXT_PUBLIC_BACKEND_URL}/shops/${storeId}`;
+
+
+    const response = await fetch(BackApi, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      cache: "no-store",
+    });
+    if (!response.ok) {
+    console.error(`HTTP Error Status: ${response.status} - ${response.statusText}`);
+
+      if (response.status === 401 || response.status === 403) {
+        throw new Error("Unauthorized access. Please log in again.");
+      }
+      throw new Error("Failed to fetch IHS data");
+    }
+    const data = await response.json();
+    console.log('%%%% FILE fetchShopMatixData: ', data)
+    return data;
+  }
