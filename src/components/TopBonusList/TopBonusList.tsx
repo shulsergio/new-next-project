87e9e1b87ff9ac71d221e13786406441c6d581ce
Sergey Shulga <shul.sergio@gmail.com>
2025-08-03 23:36:40 +0300

    add table data bonuses

--- src/components/TopBonusList/TopBonusList.tsx
+++ src/components/TopBonusList/TopBonusList.tsx
@@ -1,89 +1,55 @@
-'use client';
+import css from "./TopBonusList.module.css";
 
-import { useEffect, useState } from "react";
-import Loader from "../Loader/Loader";
-import toast from "react-hot-toast";
-import { useSession } from "next-auth/react";
-
-import css from './TopBonusList.module.css';
-
-interface TopBonusesJSON{
-    "Site Id": string;
-    "Account": string; 
-    "Item": string;
-    "SO QTY": number; 
-    "Week": string;
-    "UAH": number; 
-    "Type": string;
+export interface categorySchema {
+  account: string;
+  bonus: number;
+  item: string;
+  day: string;
+  week: string;
+  prd: string;
+  soqty: number;
 }
 
-export default function TopBonusList() {
+export interface shopBonusesProps {
+  shopBonuses: categorySchema[];
+  sessionCategory: string;
+}
 
-    const [isLoading, setIsLoading]=useState(true);
-    const [error, setError]= useState<string| null>(null);
-    const [topBouses, setTopBonuses]=useState<TopBonusesJSON[]>([]);
+export default function TopBonusList({
+  shopBonuses,
+  sessionCategory,
+}: shopBonusesProps) {
+  const filteredShopBonuses =
+    sessionCategory === "DA"
+      ? shopBonuses.filter((item) => item.prd === "SDA" || item.prd === "MDA")
+      : shopBonuses.filter((item) => item.prd === sessionCategory);
 
-const { data: session, status } = useSession();
-console.log('//// data: session-', session);
-console.log('//// data: status-', status);
-    useEffect(() => {
-            if (status === 'loading') {
-      setIsLoading(true); 
-      return;
-    }
-
-    const userShopId = session?.user?.shop ?? '';
-    const userType = session?.user?.userType ?? '';
-
-    if (status !== 'authenticated' || !userShopId || userShopId === '' || !userType || userType === '') { 
-      setIsLoading(false); 
-      setError("error");
-      setTopBonuses([]); 
-      return; 
-    }
-
-    const fetchJsonData = async () => {
-        try {
-            setIsLoading(true);
-            setError(null);
-
-        const Url = `/api/read-json?shopId=${userShopId}&userType=${userType}`;
-        console.log("//// Полный URL:", Url);
-
-        const response = await fetch(Url); 
-
-        if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error('Failed to fetch JSON data:',errorData);
-        }
-        const data: TopBonusesJSON[] = await response.json();
-        setTopBonuses(data);
-            } catch (error) {
-                console.log('Error fetching JSON- ',error)
-                toast.error('Error fetching JSON');
-            }
-            finally {
-                setIsLoading(false);
-            }
-    
-    };
-        fetchJsonData();
-    },[session, status]);
-
-     if (isLoading) {
-    return <Loader isLoading={true} />;
-  }
-
-    if (error) {
-    return toast.error(error);
-  }
-return ( <ul>
-    <p className={`${css.addtextbox} ${css.textbox}`}><span className={css.item}> Item </span><span className={css.qty}>SO QTY</span><span className={css.week}>Week</span><span className={css.uah}>UAH</span></p>
-{topBouses.map((bonus, index) => (
-    <li key={index}>
-        <p className={css.textbox}><span className={css.item}> {bonus['Item']}</span> <span className={css.qty}>{bonus["SO QTY"]}</span><span className={css.week}>{bonus["Week"]}</span><span className={css.uah}>{bonus["UAH"]}</span></p>
-    </li>
-))}
-</ul>
-)
+  return (
+    <>
+      <div className={css.tableWrapper}>
+        <table className={css.table}>
+          <thead>
+            <tr>
+              <th>SKU</th>
+              <th>Qty</th>
+              <th>Bonus</th>
+              <th>day</th>
+              <th>week</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredShopBonuses.map((item, index) => (
+              <tr key={index}>
+                <td>{item.item}</td>
+                <td>{item.soqty}</td>
+                <td>{item.bonus}</td>
+                <td>{item.day}</td>
+                <td>{item.week}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 }
