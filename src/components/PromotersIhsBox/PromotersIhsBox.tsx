86b02e48a29a5a3644f1f7d833af9efb9e8bfead
Sergey Shulga <shul.sergio@gmail.com>
2025-08-13 23:21:39 +0300

    Update PromotersIhsBox.tsx

--- src/components/PromotersIhsBox/PromotersIhsBox.tsx
+++ src/components/PromotersIhsBox/PromotersIhsBox.tsx
@@ -1,52 +1,62 @@
-
 import css from "./PromotersIhsBox.module.css";
 
-interface Category{
-    category: string;
-    share: number;
+interface Category {
+  category: string;
+  share: number;
 }
 
-export interface IhsDataItem{
-categories: Category[]; 
-    week: string;
-    year: number;
+export interface IhsDataItem {
+  categories: Category[];
+  week: string;
+  year: number;
 }
 
 interface PromotersIhsBoxProps {
-    IhsShopsData: IhsDataItem[]; 
-    sessionCategory: string;
+  IhsShopsData: IhsDataItem[];
+  sessionCategory: string;
 }
 
-export default function PromotersIhsBox({ IhsShopsData, sessionCategory }: PromotersIhsBoxProps) {
+export default function PromotersIhsBox({
+  IhsShopsData,
+  sessionCategory,
+}: PromotersIhsBoxProps) {
+  console.log("&&& PromotersIhsBox- ", IhsShopsData);
+  console.log("&&& typeof IhsShopsData:", typeof IhsShopsData);
+  console.log(
+    "&&&  IhsShopData Array.isArray(IhsShopsData):",
+    Array.isArray(IhsShopsData)
+  );
+  const weekNumberofMonth = ["w32", "w33", "w34", "w35"]; // !!!! WEEEKS !!!!   !!!! WEEEKS !!!!
 
-console.log('&&& PromotersIhsBox- ',IhsShopsData);
-    console.log('&&& typeof IhsShopsData:', typeof IhsShopsData);
-    console.log('&&&  IhsShopData Array.isArray(IhsShopsData):', Array.isArray(IhsShopsData));
-const weekNumberofMonth=["w27","w28","w29","w30","w31"];  // !!!! WEEEKS !!!!   !!!! WEEEKS !!!!  
-
-    const weeksData = weekNumberofMonth.map(weekN=>{
-        const newarray=IhsShopsData.find(item=> item.week === weekN);
-        console.log('&&& PromotersIhsBox- newarray - ',newarray);
-if(newarray){
-    const IhsInCategory= newarray.categories.find(catItem=> catItem.category === sessionCategory);
-    console.log('&&& PromotersIhsBox- IhsInCategory - ',IhsInCategory);
-    return IhsInCategory ? parseFloat((100*IhsInCategory.share).toFixed(1)) : 0;
-}
-        return 0;
-
+  const weeksData = weekNumberofMonth.map((weekN) => {
+    const newarray = IhsShopsData.find((item) => item.week === weekN);
+    console.log("&&& PromotersIhsBox- newarray - ", newarray);
+    if (newarray) {
+      const IhsInCategory = newarray.categories.find(
+        (catItem) => catItem.category === sessionCategory
+      );
+      console.log("&&& PromotersIhsBox- IhsInCategory - ", IhsInCategory);
+      return IhsInCategory
+        ? parseFloat((100 * IhsInCategory.share).toFixed(1))
+        : 0;
     }
-);
+    return 0;
+  });
 
-    return (
-
-        <div className={css.mainBox}>
-<ul className={css.list}>
-{weekNumberofMonth.map((item, index)=><li key={index}>{item}</li>)}
-</ul>
-    <ul className={css.list}>
-{weeksData.map((item, index)=><li  key={index} className={css.ihsData}>{item}%</li>)}
-        </ul>
-        </div>
-    )
-
-};
+  return (
+    <div className={css.mainBox}>
+      <ul className={css.list}>
+        {weekNumberofMonth.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+      <ul className={css.list}>
+        {weeksData.map((item, index) => (
+          <li key={index} className={css.ihsData}>
+            {item}%
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
