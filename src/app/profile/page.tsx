1fd38b5274a367612f4a8a695ac39029ad25d04f
Sergey Shulga <shul.sergio@gmail.com>
2025-09-03 23:56:38 +0300

    add data for "av.prom" + add motivation page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -5,6 +5,8 @@ import { ProfileUserBox } from "@/components/ProfileUserBox/ProfileUserBox";
 import ComponentWrapper from "@/components/ComponentWrapper/ComponentWrapper";
 import ButtonBox from "@/components/ButtonBox/ButtonBox";
 import { ProfileUserShopBox } from "@/components/ProfileUserShopBox/ProfileUserShopBox";
+import { authConfig } from "../configs/authConfig";
+import { getServerSession } from "next-auth";
 // import FirstModalData from "@/components/Modal/FirstModalData/page";
 
 /**
@@ -16,28 +18,39 @@ import { ProfileUserShopBox } from "@/components/ProfileUserShopBox/ProfileUserS
  * @return {*}
  */
 export default async function Profile() {
-  // const session = await getServerSession(authConfig);
+  const session = await getServerSession(authConfig);
 
-  // const userProfile = session?.user;
+  const userProfile = session?.user.mcsId;
+  console.log("User Profile DATA IN PROFILE PAGE:", userProfile);
   return (
     <div className={css.container}>
       {/* <FirstModalData /> */}
-      <ProfileUserBox />
-      <ProfileUserShopBox />
-      <ComponentWrapper>
-        <ButtonBox option="link" href="user/plans">
-          Promoter Plans
-        </ButtonBox>
-        <ButtonBox option="link" href="user/focus-models">
-          Focus Models
-        </ButtonBox>
-        <ButtonBox option="link" href="user/shopMatrix">
-          Shop matrix
-        </ButtonBox>
-        <ButtonBox option="link" href="user/competitors">
-          Competitors
-        </ButtonBox>
-      </ComponentWrapper>
+
+      {userProfile === "av.prom" ? (
+        <>
+          <ProfileUserBox />
+          <ProfileUserShopBox />
+        </>
+      ) : (
+        <>
+          <ProfileUserBox />
+          <ProfileUserShopBox />
+          <ComponentWrapper>
+            <ButtonBox option="link" href="user/plans">
+              Promoter Plans
+            </ButtonBox>
+            <ButtonBox option="link" href="user/motivation">
+              Motivation
+            </ButtonBox>
+            <ButtonBox option="link" href="user/focus-models">
+              Focus Models
+            </ButtonBox>
+            <ButtonBox option="link" href="user/shopMatrix">
+              Shop matrix
+            </ButtonBox>
+          </ComponentWrapper>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/app/user/motivation/page.tsx
@@ -0,0 +1,6 @@
+import TextBox from "@/components/TextBox/TextBox";
+// import css from "./page.module.css";
+
+export default function UsersMotivationPage() {
+  return <TextBox option="text">{"coming soon"}</TextBox>;
+}
