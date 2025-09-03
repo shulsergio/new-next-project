fa8723e78e577ce03bd2b0857d747f12b73d6ad3
Sergey Shulga <shul.sergio@gmail.com>
2025-09-04 00:48:57 +0300

    add MotivationBox only for AV proms

--- src/app/user/motivation/page.tsx
+++ src/app/user/motivation/page.tsx
@@ -1,6 +1,21 @@
+import { authConfig } from "@/app/configs/authConfig";
+import MotivationBox from "@/components/MotivationBox/page";
 import TextBox from "@/components/TextBox/TextBox";
+import { getServerSession } from "next-auth";
 // import css from "./page.module.css";
 
-export default function UsersMotivationPage() {
-  return <TextBox option="text">{"coming soon"}</TextBox>;
+export default async function UsersMotivationPage() {
+  const session = await getServerSession(authConfig);
+
+  const userProfile = session?.user.userType;
+  console.log("User Profile DATA IN COMPETITORS PAGE:", userProfile);
+  return (
+    <>
+      {userProfile !== "AV" ? (
+        <TextBox option="text">{"No motivation now"}</TextBox>
+      ) : (
+        <MotivationBox />
+      )}
+    </>
+  );
 }
---
+++ src/components/MotivationBox/page.tsx
@@ -0,0 +1,9 @@
+export default function MotivationBox() {
+  return (
+    <>
+      <p>Motivation Box Server Component</p>
+      <p>select cluster</p>
+      <p>show table</p>
+    </>
+  );
+}
