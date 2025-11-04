94a2fcd0197db25e3726948fea085ebd06919ee2
Sergey Shulga <shul.sergio@gmail.com>
2025-11-05 01:26:44 +0200

    change css in admin part table

--- src/components/Tables/PromotersTable/PromotersTable.tsx
+++ src/components/Tables/PromotersTable/PromotersTable.tsx
@@ -18,20 +18,28 @@ export default function PromotersTable({ promoters }: PromotersTableProps) {
         <thead>
           <tr>
             <th>Region</th>
+            <th>Chain</th>
             <th>Name</th>
             <th>MCS id</th>
             <th>Type</th>
             <th>Hired</th>
+            <th>Dismiss</th>
           </tr>
         </thead>
         <tbody>
           {promoters.map((promoter) => (
             <tr key={promoter._id}>
               <td>{promoter.region}</td>
+              <td>{promoter.region}</td>
               <td>{promoter.name}</td>
               <td>{promoter.mcsId}</td>
               <td>{promoter.userType}</td>
               <td>{promoter.DateOfHired}</td>
+              <td>
+                {Number(promoter.DateOfFired) === 0
+                  ? "-"
+                  : promoter.DateOfFired}
+              </td>
             </tr>
           ))}
         </tbody>
