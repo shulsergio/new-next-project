7a03b6c2eaef4646c32bed7991bd3d7c22e2d2ca
Sergey Shulga <shul.sergio@gmail.com>
2025-11-05 02:48:15 +0200

    change focus models visible

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,7 +2,7 @@
 export const PRD_AV_DATA = ["MONITOR", "Sound Device", "TV", "TV projector"];
 export const PRD_DA_DATA = ["B-in", "DW", "MWO", "REF", "VC", "WM"];
 
-export const MONTH_DATA = ["09.2025", "10.2025"];
+export const MONTH_DATA = ["m09y2025", "m10y2025", "m11w45-47", "m11w48"];
 
 // for Admin Plans Page
 
