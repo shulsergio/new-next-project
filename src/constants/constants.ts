76462fa2bf9fe5783847f1a762b9c627900d1857
Sergey Shulga <shul.sergio@gmail.com>
2026-02-20 18:19:56 +0200

    Update constants.ts

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -14,4 +14,4 @@ export const PROMS_TYPE_SELECT = ['AV', 'DA', 'MDA', 'SDA'];
 
 export const UniformSizeData = ["XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL"];
 
-export const ModalText = 'Плани, селаут та бонуси оновлено по 15.02';
+export const ModalText = 'Плани, селаут та бонуси оновлено по 19.02';
