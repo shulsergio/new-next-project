7f1cfca06bb08ecc45f4e36bb4e61a208ce46652
Sergey Shulga <shul.sergio@gmail.com>
2026-03-06 19:00:25 +0200

    Update constants.ts

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,7 +2,7 @@
 export const PRD_AV_DATA = ["MONITOR", "Sound Device", "TV", "TV projector"];
 export const PRD_DA_DATA = ["B-in", "DW", "MWO", "REF", "VC", "WM"];
 
-export const MONTH_DATA = ["m09y2025", "m10y2025", "m11w45-47", "m11w48", "m12y2025",  "m01y2026",  "m02y2026"];
+export const MONTH_DATA = ["m09y2025", "m10y2025", "m11w45-47", "m11w48", "m12y2025", "m01y2026", "m02y2026"];
 
 // for Admin Plans Page
 
@@ -14,4 +14,4 @@ export const PROMS_TYPE_SELECT = ['AV', 'DA', 'MDA', 'SDA'];
 
 export const UniformSizeData = ["XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL"];
 
-export const ModalText = 'Плани, селаут та бонуси оновлено за місяць, по 01.03';
+export const ModalText = 'Фінально селаут та бонуси оновлено за весь 02 місяць';
