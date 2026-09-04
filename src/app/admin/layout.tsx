cd06265046cbb7a1322fadfb6d6e61adb6a29e90
Sergey Shulga <shul.sergio@gmail.com>
2026-09-04 19:10:23 +0300

    Update layout.tsx

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,47 +1,21 @@
-"use client";
+import { getServerSession } from "next-auth";
+import { authConfig } from "@/app/configs/authConfig";
+import { redirect } from "next/navigation";
 
-import React, { useEffect } from "react";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import Loader from "@/components/Loader/Loader";
-
-export default function AdminLayout({
+export default async function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { data: session, status } = useSession();
-  const router = useRouter();
-
-  useEffect(() => {
-    // 1. Ждем завершения проверки сессии
-    if (status === "loading") return;
-
-    const isAdmin =
-      session?.user?.role === "admin" ||
-      session?.user?.permissions?.canAccessAdminPanel === true;
-
-    if (status === "unauthenticated" || !isAdmin) {
-      router.push("/signin?error=unauthorized");
-    }
-  }, [status, session, router]);
-
-  if (status === "loading") {
-    return (
-      <div className="flex h-screen items-center justify-center">
-        <Loader isLoading={true} />
-      </div>
-    );
-  }
+  const session = await getServerSession(authConfig);
 
   const isAdmin =
     session?.user?.role === "admin" ||
     session?.user?.permissions?.canAccessAdminPanel === true;
 
-  if (status === "unauthenticated" || !isAdmin) {
-    return null;
+  if (!session || !isAdmin) {
+    redirect("/signin?error=unauthorized");
   }
 
-  // Если всё отлично — рендерим макет админки и текущую страницу
   return <div className="admin-layout">{children}</div>;
 }
